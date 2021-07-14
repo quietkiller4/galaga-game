646c3b166415e64fdf1ea57e6ae175e3cfbdd684
quietkiller4 <kevinkellerjr@gmail.com>
2021-07-14 09:23:21 -0400

    Galaga starting main

---
+++ Main.py
@@ -0,0 +1,14 @@
+import pygame
+import time
+import math
+import random
+
+pygame.init()
+win_width = 800
+win_hieght = 600
+win = pygame.display.set_mode((win_width, win_hieght))
+done = False
+font_obj = pygame.font.SysFont("Courier New", 16)
+
+pygame.display.flip()
+pygame.quit()
